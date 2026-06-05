3211ea4eba626c793a74f869fe3f28774cb93374
Maneicel <kimyoon.dev@gmail.com>
2026-06-05 16:47:38 +0900

    [feature/40] 메타데이터 설정 + 청킹

--- backend/app/utils/chunker.py
+++ backend/app/utils/chunker.py
@@ -1,4 +1,32 @@
-# TODO: 텍스트 청킹 유틸 구현
+from pydantic import BaseModel
+from app.config import get_settings
 
-def chunk_text(text: str, chunk_size: int = 500, chunk_overlap: int = 50) -> list[str]:
-    pass
+settings = get_settings()
+
+class ChunkMetadata(BaseModel):
+    document_id: str
+    user_id: str
+    access_type: str
+    filename: str
+    page_number: int
+    chunk_index: int
+    chunk_text: str
+
+
+class ChunkInput(BaseModel):
+    raw_text: str
+    metadata: ChunkMetadata
+
+# (이거 받아서) -> 이거 반환해요 :: 미리 반환값 표시해주는 문법
+def chunk_text(raw_text: str) -> list[str]:
+    words = raw_text.split()
+    chunks = []
+    start = 0
+
+    while start < len(words):
+        end = start + settings.chunk_size
+        chunk = " ".join(words[start:end])
+        chunks.append(chunk)
+        start += settings.chunk_size - settings.chunk_overlap
+
+    return chunks #청크 조각 담긴 리스트 반환 
